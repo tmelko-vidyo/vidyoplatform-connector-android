98932bd39659008d32a340fb484a67299275912a
tmelko-vidyo <tmelko@vidyo.com>
2020-04-30 23:11:15 +0300

    VidyoPlatform sample app.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# vidyoplatform-connector-android
+# Custom Layout Reference App for Android
+While the VidyoConnector provides a default layout, there is also a means to customize it to meet the needs of your UI/UX. The two modes of operation, which are mutually exclusive: Composie and Custom Layouts. This reference app demonstrates how the custom layout option could be integrated to display each remote participant seperately.
+
+Developer documentation: https://vidyo.github.io/vidyoplatform.github.io
+
+# Clone
+git clone https://github.com/Vidyo/vidyoplatform-custom-connector-android.git
+
+## Acquire VidyoClient Android SDK
+
+1. Download the latest VidyoClient Android SDK package [here](https://static.vidyo.io/latest/package/VidyoClient-AndroidSDK.zip) and unzip it.
+2. Copy the SDK package content located at */VidyoClient-AndroidSDK/lib/android* to the */app/libs/* folder.
+
+# Build and Run Application
+
+Acquire the Android Vidyo Client SDK: https://static.vidyo.io/latest/package/VidyoClient-AndroidSDK.zip
+
+#### Android Studio 3.3 | Gradle tools: 3.3.0 | Target SDK version: 28 | Min SDK version: 22
+
+- Upgrade Gradle file if neccessary
+- Download required build-tools
+- Sync/Clean/Build the project
+- Refresh gradle project
---
+++ app/src/main/java/com/vidyo/vidyoconnector/VideoConferenceActivity.java
@@ -0,0 +1,324 @@
+package com.vidyo.vidyoconnector;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import com.vidyo.VidyoClient.Connector.Connector;
+import com.vidyo.VidyoClient.Connector.ConnectorPkg;
+import com.vidyo.VidyoClient.Device.Device;
+import com.vidyo.VidyoClient.Device.LocalCamera;
+import com.vidyo.vidyoconnector.event.ControlEvent;
+import com.vidyo.vidyoconnector.event.IControlLink;
+import com.vidyo.vidyoconnector.utils.AppUtils;
+import com.vidyo.vidyoconnector.utils.Logger;
+import com.vidyo.vidyoconnector.view.ControlView;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Conference activity holding all connection and callbacks logic.
+ */
+public class VideoConferenceActivity extends FragmentActivity implements Connector.IConnect,
+        Connector.IRegisterLocalCameraEventListener, IControlLink, View.OnLayoutChangeListener {
+
+    public static final String PORTAL_KEY = "portal.key";
+    public static final String ROOM_KEY = "room.key";
+    public static final String PIN_KEY = "pin.key";
+    public static final String NAME_KEY = "name.key";
+
+    private FrameLayout videoView;
+    private ControlView controlView;
+    private View progressBar;
+
+    private Connector connector;
+    private LocalCamera lastSelectedLocalCamera;
+
+    private AtomicBoolean isCameraDisabledForBackground = new AtomicBoolean(false);
+    private AtomicBoolean isDisconnectAndQuit = new AtomicBoolean(false);
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+
+        if (connector != null) {
+            ControlView.State state = controlView.getState();
+            connector.setMode(Connector.ConnectorMode.VIDYO_CONNECTORMODE_Foreground);
+
+            connector.setCameraPrivacy(state.isMuteCamera());
+            connector.setMicrophonePrivacy(state.isMuteMic());
+            connector.setSpeakerPrivacy(state.isMuteSpeaker());
+        }
+
+        if (connector != null && lastSelectedLocalCamera != null && isCameraDisabledForBackground.getAndSet(false)) {
+            connector.selectLocalCamera(lastSelectedLocalCamera);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (connector != null) {
+            connector.setMode(Connector.ConnectorMode.VIDYO_CONNECTORMODE_Background);
+
+            connector.setCameraPrivacy(true);
+            connector.setMicrophonePrivacy(true);
+            connector.setSpeakerPrivacy(true);
+        }
+
+        if (!isFinishing() && connector != null && !controlView.getState().isMuteCamera() && !isCameraDisabledForBackground.getAndSet(true)) {
+            connector.selectLocalCamera(null);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_conference);
+
+        ConnectorPkg.initialize();
+        ConnectorPkg.setApplicationUIContext(this);
+
+        videoView = findViewById(R.id.video_frame);
+
+        progressBar = findViewById(R.id.progress);
+        progressBar.setVisibility(View.GONE);
+
+        controlView = findViewById(R.id.control_view);
+        controlView.registerListener(this);
+
+        connector = new Connector(videoView, Connector.ConnectorViewStyle.VIDYO_CONNECTORVIEWSTYLE_Default, 8,
+                "*@VidyoClient info@VidyoConnector info warning", AppUtils.configLogFile(this), 0);
+        Logger.i("Connector instance has been created.");
+
+        controlView.showVersion(connector.getVersion());
+
+        connector.registerLocalCameraEventListener(this);
+
+        /* Await view availability */
+        videoView.addOnLayoutChangeListener(this);
+    }
+
+    @Override
+    public void onLayoutChange(View view, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+        view.removeOnLayoutChangeListener(this);
+
+        int width = view.getWidth();
+        int height = view.getHeight();
+
+        connector.showViewAt(view, 0, 0, width, height);
+        Logger.i("Show View at: " + width + ", " + height);
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        updateView();
+    }
+
+    @Override
+    public void onSuccess() {
+        runOnUiThread(() -> {
+            Toast.makeText(VideoConferenceActivity.this, R.string.connected, Toast.LENGTH_SHORT).show();
+            progressBar.setVisibility(View.GONE);
+
+            controlView.connectedCall(true);
+            controlView.updateConnectionState(ControlView.ConnectionState.CONNECTED);
+            controlView.disable(false);
+        });
+    }
+
+    @Override
+    public void onFailure(final Connector.ConnectorFailReason connectorFailReason) {
+        if (connector != null) connector.unregisterResourceManagerEventListener();
+
+        runOnUiThread(() -> {
+            Toast.makeText(VideoConferenceActivity.this, connectorFailReason.name(), Toast.LENGTH_SHORT).show();
+            progressBar.setVisibility(View.GONE);
+
+            controlView.connectedCall(false);
+            controlView.updateConnectionState(ControlView.ConnectionState.FAILED);
+            controlView.disable(false);
+
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        });
+    }
+
+    @Override
+    public void onDisconnected(Connector.ConnectorDisconnectReason connectorDisconnectReason) {
+        if (connector != null) connector.unregisterResourceManagerEventListener();
+
+        runOnUiThread(() -> {
+            Toast.makeText(VideoConferenceActivity.this, R.string.disconnected, Toast.LENGTH_SHORT).show();
+            progressBar.setVisibility(View.GONE);
+
+            controlView.connectedCall(false);
+            controlView.updateConnectionState(ControlView.ConnectionState.DISCONNECTED);
+            controlView.disable(false);
+
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+            /* Wrap up the conference */
+            if (isDisconnectAndQuit.get()) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onControlEvent(ControlEvent event) {
+        if (connector == null) return;
+
+        switch (event.getCall()) {
+            case CONNECT_DISCONNECT:
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+                progressBar.setVisibility(View.VISIBLE);
+                controlView.disable(true);
+                boolean state = (boolean) event.getValue();
+                controlView.updateConnectionState(state ? ControlView.ConnectionState.CONNECTING : ControlView.ConnectionState.DISCONNECTING);
+
+                if (state) {
+                    Intent intent = getIntent();
+
+                    String portal = intent.getStringExtra(PORTAL_KEY);
+                    String room = intent.getStringExtra(ROOM_KEY);
+                    String pin = intent.getStringExtra(PIN_KEY);
+                    String name = intent.getStringExtra(NAME_KEY);
+
+                    Logger.i("Start connection: %s, %s, %s, %s", portal, room, pin, name);
+                    connector.connectToRoomAsGuest(portal, name, room, pin, this);
+                } else {
+                    if (connector != null) connector.disconnect();
+                }
+                break;
+            case MUTE_CAMERA:
+                boolean cameraPrivacy = (boolean) event.getValue();
+                connector.setCameraPrivacy(cameraPrivacy);
+
+                if (cameraPrivacy) {
+                    connector.selectLocalCamera(null);
+                } else {
+                    if (lastSelectedLocalCamera != null)
+                        connector.selectLocalCamera(lastSelectedLocalCamera);
+                    else
+                        connector.selectDefaultCamera();
+                }
+                break;
+            case MUTE_MIC:
+                connector.setMicrophonePrivacy((boolean) event.getValue());
+                break;
+            case MUTE_SPEAKER:
+                connector.setSpeakerPrivacy((boolean) event.getValue());
+                break;
+            case CYCLE_CAMERA:
+                connector.cycleCamera();
+                break;
+            case DEBUG_OPTION:
+                boolean value = (boolean) event.getValue();
+                if (value) {
+                    connector.enableDebug(7776, "");
+                } else {
+                    connector.disableDebug();
+                }
+
+                Toast.makeText(VideoConferenceActivity.this, getString(R.string.debug_option) + value, Toast.LENGTH_SHORT).show();
+                break;
+            case SEND_LOGS:
+                AppUtils.sendLogs(this);
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (connector == null) {
+            Logger.e("Connector is null!");
+            finish();
+            return;
+        }
+
+        Connector.ConnectorState state = connector.getState();
+
+        if (state == Connector.ConnectorState.VIDYO_CONNECTORSTATE_Idle || state == Connector.ConnectorState.VIDYO_CONNECTORSTATE_Ready) {
+            super.onBackPressed();
+        } else {
+            /* You are still connecting or connected */
+            Toast.makeText(this, "You have to disconnect or await connection first", Toast.LENGTH_SHORT).show();
+
+            /* Start disconnection if connected. Quit afterward. */
+            if (state == Connector.ConnectorState.VIDYO_CONNECTORSTATE_Connected && !isDisconnectAndQuit.get()) {
+                isDisconnectAndQuit.set(true);
+                onControlEvent(new ControlEvent<>(ControlEvent.Call.CONNECT_DISCONNECT, false));
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (controlView != null) controlView.unregisterListener();
+
+        if (connector != null) {
+            connector.unregisterLocalCameraEventListener();
+
+            connector.hideView(videoView);
+
+            connector.disable();
+            connector = null;
+        }
+
+        ConnectorPkg.uninitialize();
+        ConnectorPkg.setApplicationUIContext(null);
+
+        Logger.i("Connector instance has been released.");
+    }
+
+    @Override
+    public void onLocalCameraAdded(LocalCamera localCamera) {
+        Logger.i("Local camera added.");
+    }
+
+    @Override
+    public void onLocalCameraSelected(final LocalCamera localCamera) {
+        Logger.i(VideoConferenceActivity.class, "Local camera selected");
+
+        if (localCamera != null) {
+            this.lastSelectedLocalCamera = localCamera;
+        }
+    }
+
+    @Override
+    public void onLocalCameraStateUpdated(LocalCamera localCamera, Device.DeviceState deviceState) {
+
+    }
+
+    @Override
+    public void onLocalCameraRemoved(LocalCamera localCamera) {
+        Logger.i("Local camera removed.");
+    }
+
+    private void updateView() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+
+        FrameLayout.LayoutParams videoViewParams = new FrameLayout.LayoutParams(width, height);
+        videoView.setLayoutParams(videoViewParams);
+
+        videoView.addOnLayoutChangeListener(this);
+        videoView.requestLayout();
+    }
+}
